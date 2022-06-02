e3e9bdac02f5d8050a5664ff3f7e83481d5a341d
dmi <dmirtray@gmail.com>
2022-06-02 17:41:22 +0300

    movies by countries

--- src/main/java/mflix/api/daos/MovieDao.java
+++ src/main/java/mflix/api/daos/MovieDao.java
@@ -14,6 +14,11 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.in;
+import static com.mongodb.client.model.Projections.fields;
+import static com.mongodb.client.model.Projections.include;
+
 @Component
 public class MovieDao extends AbstractMFlixDao {
 
@@ -62,7 +67,7 @@ public class MovieDao extends AbstractMFlixDao {
 
         List<Bson> pipeline = new ArrayList<>();
         // match stage to find movie
-        Bson match = Aggregates.match(Filters.eq("_id", new ObjectId(movieId)));
+        Bson match = Aggregates.match(eq("_id", new ObjectId(movieId)));
         pipeline.add(match);
         // TODO> Ticket: Get Comments - implement the lookup stage that allows the comments to
         // retrieved with Movies.
@@ -118,10 +123,16 @@ public class MovieDao extends AbstractMFlixDao {
      */
     public List<Document> getMoviesByCountry(String... country) {
 
-        Bson queryFilter = new Document();
-        Bson projection = new Document();
+        Bson queryFilter = in("countries", country);
+        Bson projection = new Document("title", 1);
         //TODO> Ticket: Projection - implement the query and projection required by the unit test
         List<Document> movies = new ArrayList<>();
+
+        moviesCollection
+                .find(queryFilter)
+                .projection(projection)
+                .iterator()
+                .forEachRemaining(movies::add);
 
         return movies;
     }
